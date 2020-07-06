c45067f16595048033fe449e769a53794d65b95c
ZengJing <3051850092@qq.com>
2020-07-06 07:23:07 +0000

    用户可以编辑话题

--- app/Policies/TopicPolicy.php
+++ app/Policies/TopicPolicy.php
@@ -7,11 +7,12 @@ use App\Models\Topic;
 
 class TopicPolicy extends Policy
 {
+	//将只允许作者对话题有编辑权限
     public function update(User $user, Topic $topic)
-    {
-        // return $topic->user_id == $user->id;
-        return true;
-    }
+	{
+	 return $topic->user_id == $user->id;
+	}
+
 
     public function destroy(User $user, Topic $topic)
     {
