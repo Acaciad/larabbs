60f0b228712af2d14048d7990839bf708bbeaaca
ZengJing <3051850092@qq.com>
2020-07-07 01:49:12 +0000

    用户可以发表评论

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -11,19 +11,18 @@ class ReplyObserver
 {
     public function creating(Reply $reply)
     {
-        //字段缓存
-        $reply->topic->reply_count = $reply->topic->replies->count();
- 		$reply->topic->save();
+       
     }
 
     public function updating(Reply $reply)
     {
         //
     }
-    //话题下每新增一条回复，应该 +1 
+    //话题下每新增一条回复，应该 累加 
     public function created(Reply $reply)
-	{
-	 	$reply->topic->increment('reply_count', 1);
-	}
+    {
+        $reply->topic->reply_count = $reply->topic->replies->count();
+        $reply->topic->save();
+    }
 
 }
