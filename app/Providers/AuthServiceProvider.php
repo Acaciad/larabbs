587c42b225521b7b17c2635586e77f03519db5f7
ZengJing <3051850092@qq.com>
2020-07-08 02:49:43 +0000

    站点权限部署

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -31,5 +31,9 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         //
+        \Horizon::auth(function ($request) {
+         // 是否是站长
+         return \Auth::user()->hasRole('Founder');
+         });
     }
 }
