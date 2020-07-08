1641681a62ada08164e7a80d2bffac35346d3c02
ZengJing <3051850092@qq.com>
2020-07-08 14:20:10 +0000

    注册用户默认头像

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -18,4 +18,11 @@ class UserObserver
     {
         //
     }
+    public function saving(User $user)
+	{
+	 // 这样写扩展性更高，只有空的时候才指定默认头像
+		 if (empty($user->avatar)) {
+		 $user->avatar = 'http://larabbs.test/uploads/images/avatars/V0IPWQ7EHBTds0zaPUeNoGsRU9TnLqyh.jpg';
+		 }
+	}
 }
