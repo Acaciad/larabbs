2adbbc4d7a9689682933c670575a24a44d3711b6
ZengJing <3051850092@qq.com>
2020-07-11 02:14:11 +0000

    完善

--- resources/views/shared/_messages.blade.php
+++ resources/views/shared/_messages.blade.php
@@ -1,14 +1,9 @@
 @foreach (['danger', 'warning', 'success', 'info'] as $msg)
- @if(session()->has($msg))
- <div class="flash-message">
- <p class="alert alert-{{ $msg }}">
- {{ session()->get($msg) }}
- </p>
- </div>
-session()->flash('success', 'This is a success alert—check it out!');
-session()->flash('danger', 'This is a danger alert—check it out!');
-session()->flash('warning', 'This is a warning alert—check it out!');
-session()->flash('info', 'This is a info alert—check it out!');
-
- @endif
+  @if(session()->has($msg))
+    <div class="flash-message">
+      <p class="alert alert-{{ $msg }}">
+        {{ session()->get($msg) }}
+      </p>
+    </div>
+  @endif
 @endforeach
